45caf7bfd73ecf48858c62298b0447ce29a5c406
zyzisyz <zyziszy@foxmail.com>
2020-05-09 10:31:50 +0800

    update

--- leetcode/100.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        int res = haystack.find(needle);
-        if(res!=haystack.npos) return res;
-        return -1;
-    }
-};
