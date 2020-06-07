55221a55515231182b6bd133edbdb55501a565fc
zyzisyz <zyziszy@foxmail.com>
2020-06-07 20:13:00 +0800

    update

--- practice/0849.cpp
+++ practice/0849.cpp
@@ -1,32 +1,33 @@
 #include<iostream>
 #include<vector>
 
+const int INF = 10005;
+
 using namespace std;
 
-const int INF = 1e4+5;
-
-
-vector<vector<int>> g;
-vector<int> dist;
-vector<bool> visited;
 int n, m;
+vector<vector<int>> g;
+vector<bool> visited;
+vector<int> dist;
 
-int dijsktra(){
+int dijskra(){
 	dist[1] = 0;
+
 	for(int i=0; i<n; i++){
 		int t = -1;
 		int cmp = INF;
 		for(int j=1; j<=n; j++){
-			if(visited[j]==false && cmp>dist[j]){
+			if(visited[j]==false && dist[j]<cmp){
+				t = j;
 				cmp = dist[j];
-				t = j; 
 			}
 		}
-		if(t==-1) return -1;
+		if(t == -1) return -1;
+
 		visited[t] = true;
 
 		for(int j=1; j<=n; j++){
-			if(visited[j]==false && g[t][j]!=INF){
+			if(g[t][j]!=INF){
 				dist[j] = min(dist[j], dist[t]+g[t][j]);
 			}
 		}
@@ -37,13 +38,15 @@ int dijsktra(){
 int main(void){
 	cin>>n>>m;
 	g.assign(n+1, vector<int>(n+1, INF));
-	dist.assign(n+1, INF);
 	visited.assign(n+1, false);
+	dist.assign(n+1, INF);
 
 	for(int i=0; i<m; i++){
 		int x, y, z;
 		cin>>x>>y>>z;
-		g[x][y] = min(g[x][y], z);
+		g[x][y] = min(z, g[x][y]);
 	}
-	cout<<dijsktra()<<endl;
+
+	cout<<dijskra()<<endl;
+
 }
---
+++ practice/1018.cpp
@@ -0,0 +1,33 @@
+﻿#include<cstdio>
+#include<algorithm>
+#include<iostream>
+
+using namespace std;
+
+const int inf = 1e9;
+const int N = 110;
+int w[N][N];
+int f[N][N];
+int n;
+
+int main(){
+
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            scanf("%d", &w[i][j]);
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(i == 1 && j == 1) f[i][j] = w[i][j];
+            else{
+                f[i][j] = inf;
+                if(i > 1) f[i][j] = min(f[i][j], f[i - 1][j]) + w[i][j];
+                if(j > 1) f[i][j] = min(f[i][j], f[i][j - 1]) + w[i][j];
+            }
+        }
+    }
+    printf("%d", f[n][n]);
+    return 0;
+}
